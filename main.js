398d233f5292badc880982771ea616f8c467c03a
github9088106153 <87702995+github9088106153@users.noreply.github.com>
2022-04-01 14:14:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+Webcam.set({
+    width:350,
+    height:275,
+    img_format:'png',
+    png_quality: 100,
+});
+camera= document.getElementById("camera");
+
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML="<img id='capture_image' src='"+data_uri+"'>";
+    });
+
+}
+console.log("ml5 version: ",ml5.version); 
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/J1otpfkzj/model.json",modelLoaded);
+
+function modelLoaded(){
+    console.log("model Loaded");
+}
